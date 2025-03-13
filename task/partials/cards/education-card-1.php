b8aac79cc67fa8783bc84372a32c0b7b33a25041
saskiaramadani <rmdnsaskia@gmail.com>
2025-03-13 09:34:54 +0700

    upload tugas praktikum

---
+++ task/partials/cards/education-card-1.php
@@ -0,0 +1,20 @@
+<div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-secondary fw-bolder mb-2">2018 - 2021</div>
+                                                <div class="mb-2">
+                                                    <div class="small fw-bolder">MTS Al-Mujahidin</div>
+                                                    <div class="small text-muted">Ds Kiarapandak, Kec Sukajaya, Kab Bogor Jawa Barat</div>
+                                                </div>
+                                                <div class="fst-italic">
+                                                    <div class="small text-muted"></div>
+                                                    <div class="small text-muted"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Madrasah Tsanawiyah (MTS) merupakan lembaga pendidikan yang sama dengan Sekolah Menengah Pertama(SMP).</div></div>
+                                    </div>
+                                </div>
+                            </div>
---
+++ task/partials/cards/education-card-2.php
@@ -0,0 +1,20 @@
+<div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-secondary fw-bolder mb-2">2021 - 2024</div>
+                                                <div class="mb-2">
+                                                    <div class="small fw-bolder">SMK Insan Madani</div>
+                                                    <div class="small text-muted">Ds Urug, Kec Sukajaya, Kab Bogor Jawa Barat</div>
+                                                </div>
+                                                <div class="fst-italic">
+                                                    <div class="small text-muted"></div>
+                                                    <div class="small text-muted"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Saat jenjang SMK saya mengambil jurusan Otomatisasi Tata Kelola Perkantoran (OTKP), dimana jurusan tersebut merujuk pada Administrasi dan Manajemen.</div></div>
+                                    </div>
+                                </div>
+                            </div>
