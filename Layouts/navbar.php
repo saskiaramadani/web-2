02d19be14f0020ff85e2ff83874e280886ef5984
saskiaramadani <rmdnsaskia@gmail.com>
2025-04-23 22:00:52 +0700

    upload file

--- Layouts/navbar.php
+++ Layouts/navbar.php
@@ -89,6 +89,15 @@
                         </p>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a href="?url=users" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            Dashboard
+                            <span class="right badge badge-danger">New</span>
+                        </p>
+                    </a>
+                </li>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
---
+++ views/user.php
@@ -0,0 +1,19 @@
+<section class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="card">
+                <div class="card-body">
+                   <table class="table table-bordered">
+                        <tr>
+                            <th>NO<\</th>
+                            <th>Nama</th>
+                            <th>Telepon</th>
+                            <th>Email</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </div>
+                 </div>
+            </div>
+        </div>
+     </div>
+</selection> 
