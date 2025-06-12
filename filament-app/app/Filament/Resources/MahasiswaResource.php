533772fcaa7cad94853e17edf7b0355cdc7f5b22
saskiaramadani <rmdnsaskia@gmail.com>
2025-06-12 15:20:44 +0700

    praktikum11

---
+++ filament-app/app/Filament/Resources/MahasiswaResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MahasiswaResource\Pages;
+use App\Filament\Resources\MahasiswaResource\RelationManagers;
+use App\Models\Mahasiswa;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+
+
+class MahasiswaResource extends Resource
+{
+    protected static ?string $model = Mahasiswa::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('nama')->searchable()->sortable(),
+                TextColumn::make('nim'),
+                TextColumn::make('jurusan'),
+                TextColumn::make('jenis_kelamin'),
+                TextColumn::make('agama'),
+                TextColumn::make('status')
+            ])
+            ->filters([
+                //agama
+                SelectFilter::make('agama')
+                ->options([
+                    'Islam' => 'Islam',
+                    'Kristen' => 'Kristen',
+                    'Hindu' => 'Hindu',
+                ]),
+                //jenis_kelamin
+                SelectFilter::make('jenis_kelamin')
+                ->options([
+                    'P' => 'Perempuan',
+                    'L' => 'Laki-Laki',
+                ])
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMahasiswas::route('/'),
+            'create' => Pages\CreateMahasiswa::route('/create'),
+            'edit' => Pages\EditMahasiswa::route('/{record}/edit'),
+        ];
+    }
+}
